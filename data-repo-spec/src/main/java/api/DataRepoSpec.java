13fdd6c7826e0dcdc78b3283acfec0230d5d95a3
Numulix <jbabic999@gmail.com>
2020-11-11 18:36:42 +0100

    Dokumentacija komponente za specifikaciju

--- data-repo-spec/src/main/java/api/DataRepoSpec.java
+++ data-repo-spec/src/main/java/api/DataRepoSpec.java
@@ -4,7 +4,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-
+/**
+ * Klasa koja predstavlja apstrakciju lokalne baze podataka
+ * koja cuva proizvoljne entitete u struktuiranje lokalne
+ * datoteke.
+ * 
+ * 
+ * @author Jovan Babic RN 30/18
+ * @author Filip Cmiljanovc RN 5/18
+ *
+ */
 public abstract class DataRepoSpec {
 	
 	private List<Entity> entityList;
@@ -62,6 +71,13 @@ public abstract class DataRepoSpec {
 		return true;
 	}
 	
+	/**
+	 * Proverava da li u bazi postoji entitet
+	 * sa datim imenom.
+	 * 
+	 * @param name String koji predstavlja ime entiteta
+	 * @return Vraca <code>true</code> ukoliko postoji, u suprotnom vraca <code>false</code>
+	 */
 	private boolean checkName(String name) {
 		for (Entity e: entityList) {
 			if (e.getName().equals(name)) return true;
@@ -70,16 +86,34 @@ public abstract class DataRepoSpec {
 		return false;
 	}
 	
+	/**
+	 * Dodaje entitet u listu svih entiteta prisutnih u bazi
+	 * 
+	 * @param en Predstavlja entitet koji se dodaje u bazu
+	 */
 	private void addEntity(Entity en) {
 		getEntityList().add(en);
 		save();
 	}
 	
+	/**
+	 * Brise dati entitet iz liste entiteta u bazi
+	 * 
+	 * @param en Predstavlja entitet koji se brise iz baze
+	 */
 	private void delete(Entity en) {
 		entityList.remove(entityList.indexOf(en));
 		save();
 	}
 	
+	/**
+	 * Funkcija koja vraca entitet na osnovu
+	 * zadatog jedinstvenog ID-a
+	 * 
+	 * @param id ID po kojem se entitet trazi
+	 * @return Ukoliko postoji entitet sa zadatim id-em vraca taj konkretan
+	 * entitet, u suprotnom vraca <code>null</code>
+	 */
 	public Entity findEntityByID(int id) {
 		for (Entity e: entityList) {
 			if (e.getId() == id) return e;
@@ -88,6 +122,15 @@ public abstract class DataRepoSpec {
 		return null;
 	}
 	
+	/**
+	 * Brise entitet iz liste entiteta po zadatoj
+	 * ID vrednost
+	 * 
+	 * @param id ID vrednost entiteta koji se brise
+	 * @return Vraca vrednost <code>true</code> ukoliko je
+	 * entitet sa zadatim ID-em nadjen i izbrisan, u suprotnom
+	 * vraca false ukoliko ne postoji entitet sa tim ID-em
+	 */
 	public boolean deleteByID(int id) {
 		if (!checkID(id)) {
 			delete(findEntityByID(id));
@@ -96,6 +139,15 @@ public abstract class DataRepoSpec {
 		return false;
 	}
 	
+	/**
+	 * Brise sve entitete iz liste entiteta po zadatom
+	 * imenu entiteta
+	 * 
+	 * @param name Ime entiteta koji se brisu
+	 * @return Vraca <code>true</code> ukoliko je pronadjen barem jedan
+	 * entitet sa zadatim imenom i brise sve entitete koje imaju zadato
+	 * ime, u suprotnom vraca <code>false</code>
+	 */
 	public boolean deleteByEntityName(String name) {
 		if (checkName(name)) {
 			for (Entity e: entityList) {
@@ -107,6 +159,17 @@ public abstract class DataRepoSpec {
 		return false;
 	}
 	
+	/**
+	 * Brise entitete iz liste entiteta prema zadatom imenu
+	 * entiteta i mapi atributa.
+	 * 
+	 * @param name Ime entitet koji se pretrazuje za brisanje
+	 * @param attr Mapa atributa koja sadrzi konkretne parove
+	 * kljuc-vrednost
+	 * @return Vraca <code>true</code> ukoliko su pronadjeni i izbrisani
+	 * svi entiteti po zadatom nazivu i mapi atributa, u suprotnom
+	 * vraca <code>false</code>
+	 */
 	public boolean deleteByNameAndAttr(String name, Map<String, Object> attr) {
 		if (checkName(name)) {
 			for (Entity e: entityList ) {
@@ -132,6 +195,18 @@ public abstract class DataRepoSpec {
 		return false;
 	}
 	
+	/**
+	 * Dodaje entitet u listu entiteta po konkretnim zadatim
+	 * vrednostima.
+	 * 
+	 * 
+	 * @param id ID vrednost entiteta, mora biti jedinstven
+	 * @param name Ime entiteta
+	 * @param attributes Mapa atributa entiteta
+	 * @return Vraca <code>false</code> ukoliko vec postoji
+	 * entitet sa zadatim ID-em, u suprotnom dodaje se u listu
+	 * i vraca <code>true</code>
+	 */
 	public boolean add(int id, String name, Map<String, Object> attributes) {
 		if (checkID(id)) {
 			addEntity(new Entity(id, name, attributes));
@@ -140,11 +215,26 @@ public abstract class DataRepoSpec {
 		return false;
 	}
 	
+	/**
+	 * Dodaje entitet u listu entiteta sa automatski generisanim
+	 * jedinstvenim ID-em i zadatim imenom i mapom atributa
+	 * 
+	 * @param name Ime entiteta koji se dodaje u listu
+	 * @param attributes Mapa atributa entiteta koji se dodaje u listu
+	 */
 	public void addAutoId(String name, Map<String, Object> attributes) {
 		int maxId = findMaxID();
 		addEntity(new Entity(maxId+1, name, attributes));
 	}
 	
+	/**
+	 * Uzima postojeci entitet iz baze i azurira mu vrednosti
+	 * na osnovu datih argumenata
+	 * 
+	 * @param id ID entiteta koji se azurira
+	 * @param name Ime entiteta koji moze biti novi naziv entiteta
+	 * @param attributes Mapa atributa sa dodatim ili izbrisanim vrednostima
+	 */
 	public void updateEntity(int id, String name, Map<String, Object> attributes) {
 		Entity en = findEntityByID(id);
 		en.setName(name);
@@ -152,6 +242,16 @@ public abstract class DataRepoSpec {
 		save();
 	}
 	
+	
+	/**
+	 * Dodaje entitet u mapu atributa vec postojeceg entiteta.
+	 * 
+	 * @param idEnt ID entiteta kojem se dodaje entitet kao atribut
+	 * @param attrName Ime atributa entiteta koji se dodaje
+	 * @param idAttr ID entiteta koji sluzi kao atribut
+	 * @param nameAttr Ime entiteta koji sluzi kao atribut
+	 * @param attr Mapa atributa entiteta koji sluzi kao atribut
+	 */
 	public void addEntityAsAttribute(int idEnt, String attrName, int idAttr, String nameAttr, Map<String, Object> attr) {
 		if (!checkID(idEnt)) {
 			Entity en = findEntityByID(idEnt);
@@ -160,6 +260,16 @@ public abstract class DataRepoSpec {
 		}
 	}
 	
+	/**
+	 * Filtrira zadatu listu entiteta na osnovu
+	 * zadate mape atributa.
+	 * 
+	 * @param list Lista po kojoj se vrsi filtriranje
+	 * @param args Mapa atributa po kojoj se vrsi filtriranje
+	 * @return Vraca filtriranu listu entiteta ili praznu listu
+	 * ukoliko nije pronadjen nijedan entitet sa zadatim
+	 * atributima
+	 */
 	public List<Entity> filterByArgs(List<Entity> list, Map<String, Object> args) {
 		List<Entity> returnList = new ArrayList<Entity>();
 		
@@ -184,6 +294,16 @@ public abstract class DataRepoSpec {
 		return returnList;
 	}
 	
+	
+	/**
+	 * Filtrira listu svih entiteta u bazi po zadatoj
+	 * mapi atributa.
+	 * 
+	 * @param args Mapa atributa po kojoj se vrsi filtriranje
+	 * @return Vraca listu entiteta koja sadrzi te atribute ili
+	 * praznu listu ukoliko nije pronadjen nijedan entitet sa tim
+	 * atributima
+	 */
 	public List<Entity> filterByArgs(Map<String, Object> args) {
 		List<Entity> returnList = new ArrayList<Entity>();
 		
@@ -208,6 +328,15 @@ public abstract class DataRepoSpec {
 		return returnList;
 	}
 	
+	
+	/**
+	 * Filtrira celu listu entiteta po zadatom imenu entiteta.
+	 * 
+	 * @param name Ime entiteta po kojem se vrsi filtriranje.
+	 * @return Vraca listu entiteta gde se ime entiteta poklapa
+	 * sa zadatim imenom ili praznu listu ukoliko ne postoji
+	 * entitet sa zadatim imenom ili mapo atributa
+	 */
 	public List<Entity> filterByEntityName(String name) {
 		List<Entity> returnList = new ArrayList<Entity>();
 		
@@ -218,22 +347,47 @@ public abstract class DataRepoSpec {
 		return returnList;
 	}
 	
+	/**
+	 * Filtrira listu svih entiteta po zadatom imenu
+	 * entiteta i mape atributa.
+	 * 
+	 * @param name Ime entiteta po kom se vrsi filtriranje
+	 * @param args Mapa atributa po kojoj se vrsi filtriranje
+	 * @return Vraca listu entieta filtriranu po zadatom imenu
+	 * entiteta i mapi atributa ili vraca praznu listu ukoliko
+	 * ne postoji entitet sa zadatim imenom ili mapom atributa
+	 */
 	public List<Entity> filterByNameAndArgs(String name, Map<String, Object> args) {
 		List<Entity> nameList = filterByEntityName(name);
 		
 		return filterByArgs(nameList, args);
 	}
 	
+	/**
+	 * Sortira listu svih entiteta po vrednosti ID-a
+	 * 
+	 */
 	public void sortEntityByID() {
 		Collections.sort(entityList, new SortEntityByID());
 	}
 	
+	/**
+	 * Sortira listu svih entiteta po imenu entiteta
+	 */
 	public void sortEntityByName() {
 		Collections.sort(entityList, new SortEntityByName());
 	}
 	
-	
+	/**
+	 * Cuva listu entiteta u poseban fajl u specificiranom formatu.
+	 */
 	public abstract void save();
+	
+	/**
+	 * Ucitava sve korektne fajlove
+	 * u direktorijumu gde se nalaze entiteti
+	 * i ubacuje entitete iz fajlova u listu svih entiteta.
+	 */
 	public abstract void load();
 	
 	
